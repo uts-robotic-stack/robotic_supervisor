8150ac81f814b1209ba535f41264889599acead9
dkhoanguyen <khoanguyendacdang2198@gmail.com>
2023-11-16 22:22:32 +1100

    Update code

--- pkg/types/device.go
+++ pkg/types/device.go
@@ -1,12 +1,12 @@
 package types
 
 type Device struct {
-	Status            string
-	Uuid              string
-	Type              string
-	OnlineDuration    int64
-	OsType            string
-	DeviceRole        string
-	InternetStatus    string
-	SupervisorRelease string
+	Status            string `json:"status"`
+	Uuid              string `json:"uuid"`
+	Type              string `json:"device_type"`
+	OnlineDuration    int64  `json:"online_duration"`
+	OsType            string `json:"os_type"`
+	DeviceRole        string `json:"device_role"`
+	InternetStatus    string `json:"internet_status"`
+	SupervisorRelease string `json:"supervisor_release"`
 }
--- test.sh
+++ test.sh
@@ -11,4 +11,4 @@
 #     }
 # }' localhost:8080/api/v1/watchtower/container
 
-curl -H "Authorization: Bearer robotics" --request GET -H  "Content-Type: application/json" localhost:8080/api/v1/watchtower/device-info
+curl -H "Authorization: Bearer robotics" --request GET  0.0.0.0:8080/api/v1/watchtower/device-info
