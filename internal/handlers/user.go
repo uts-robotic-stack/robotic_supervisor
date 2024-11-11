d8d947955826f81c04d23c3e12d5c45813cf2e68
dkhoanguyen <khoanguyendacdang2198@gmail.com>
2024-11-11 21:04:23 +1100

    Updated backend to add user

--- internal/handlers/user.go
+++ internal/handlers/user.go
@@ -1,27 +1,25 @@
 package handlers
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
+	"gopkg.in/yaml.v3"
 )
 
-type UserHandler struct{}
+type UserHandler struct {
+	users map[string]User
+}
 
 // User represents the user information.
 type User struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Role     string `json:"role"`
-}
-
-// In-memory user storage for demonstration purposes.
-var users = map[string]User{
-	"robotic": {Username: "robotic", Password: "admin", Role: "admin"},
+	Username string `json:"username" yaml:"username"`
+	Password string `json:"password" yaml:"password"`
+	Role     string `json:"role" yaml:"role"`
 }
 
 // Claims represents the JWT payload.
@@ -33,8 +31,34 @@ type Claims struct {
 
 var jwtKey = []byte("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InJvYm90aWMiLCJyb2xlIjoiYWRtaW4iLCJleHAiOjE3MzA2MTc5NDl9.5RHDAgo7eNv9h3FsW0ypWVQkqKlAiWc8U-V83OszR1Y")
 
+// NewUserHandler initializes UserHandler and loads users from a YAML file.
 func NewUserHandler() *UserHandler {
-	return &UserHandler{}
+	handler := &UserHandler{users: make(map[string]User)}
+
+	err := handler.loadUsersFromYAML("config/user.yaml")
+	if err != nil {
+		return nil
+	}
+
+	return handler
+}
+
+// loadUsersFromYAML reads user data from a YAML file.
+func (h *UserHandler) loadUsersFromYAML(filePath string) error {
+	yamlData, err := os.ReadFile(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to read YAML file: %v", err)
+	}
+
+	var usersData struct {
+		Users map[string]User `yaml:"users"`
+	}
+	if err := yaml.Unmarshal(yamlData, &usersData); err != nil {
+		return fmt.Errorf("failed to unmarshal YAML: %v", err)
+	}
+
+	h.users = usersData.Users
+	return nil
 }
 
 // SignInHandler handles user sign-in and returns a JWT if successful.
@@ -44,15 +68,19 @@ func (h *UserHandler) HandleUserSignIn(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request payload"})
 		return
 	}
-	fmt.Println(credentials)
-	// Authenticate user credentials.
+
 	user, err := h.authenticateUser(credentials.Username, credentials.Password)
 	if err != nil {
 		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid username or password"})
 		return
 	}
 
-	// Create a new JWT token.
+	// If the user role is "user", do not return a token
+	if user.Role == "user" {
+		c.JSON(http.StatusOK, gin.H{"message": "Authenticated as regular user"})
+		return
+	}
+
 	expirationTime := time.Now().Add(24 * time.Hour)
 	claims := &Claims{
 		Username: user.Username,
@@ -69,17 +97,19 @@ func (h *UserHandler) HandleUserSignIn(c *gin.Context) {
 		return
 	}
 
-	// Send the token to the client.
 	c.JSON(http.StatusOK, gin.H{"token": tokenString})
 }
 
-// authenticateUser verifies the user's credentials.
 func (h *UserHandler) authenticateUser(username, password string) (User, error) {
-	user, exists := users[username]
-	if !exists || user.Password != password {
-		return User{}, errors.New("invalid credentials")
+	// Iterate over all roles in the users map
+	for _, user := range h.users {
+		// For other roles, check if both username and password match
+		if user.Username == username && user.Password == password {
+			return user, nil
+		}
 	}
-	return user, nil
+	// By default return user
+	return User{Username: username, Role: "user"}, nil
 }
 
 // RoleHandler returns the role of the authenticated user.
